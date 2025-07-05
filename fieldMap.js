e88a10517a70f38ee2fb873a541e7a6ebd54416b
SNG-69 <punithv.pv@gmail.com>
2025-07-05 12:41:59 +0530

    fieldMap.js

--- fieldMap.js
+++ fieldMap.js
@@ -1,7 +1,20 @@
+// fieldMap.js
 export const FIELD_MAP = {
-  customfield_10057: "Payment Type",
-  customfield_10060: "Internal Priority",
-  customfield_10061: "Region",
-  customfield_10063: "Sales Channel",
-  customfield_10065: "Customer Segment"
+  customfield_10091: "Calling Date",
+  customfield_10095: "Sport",
+  customfield_10089: "Product Category",
+  customfield_10093: "Vendor",
+  customfield_10094: "Cost Price",
+  customfield_10085: "Date of Procurement",
+  customfield_10084: "Courier Partner",
+  customfield_10080: "Shipping Tracking Details",
+  customfield_10086: "Pickup Schedule Date",
+  customfield_10087: "Dispatch Date",
+  customfield_10088: "Delivery Date",
+  customfield_10106: "Delhivery Escalation Ticket Number",
+  customfield_10119: "RTO Delivered Date",
+  customfield_10120: "Replacement Procured Date",
+  customfield_10121: "Replacement Dispatch Date",
+  customfield_10123: "Return Delivery Date",
+  customfield_10156: "Purchase Invoice Link"
 };
